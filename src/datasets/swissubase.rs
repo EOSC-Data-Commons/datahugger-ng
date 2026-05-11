8521397bba57352248f79f79f03906e793564396
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-05-11 11:02:53 +0200

    feat (swissubase): parse JSON

--- src/datasets/swissubase.rs
+++ src/datasets/swissubase.rs
@@ -1,11 +1,99 @@
-use crate::{repo::RepoError, DatasetBackend, DirMeta, Entry};
+use crate::helper::json_extract;
+use crate::repo::Endpoint;
+use crate::{repo::RepoError, Checksum, DatasetBackend, DirMeta, Entry, FileMeta};
 use async_trait::async_trait;
 use exn::{Exn, ResultExt};
 use reqwest::{Client, StatusCode};
 use serde_json::Value as JsonValue;
 use std::any::Any;
+use std::path::Path;
 use std::str::FromStr;
 use url::Url;
+
+fn analyse_json(json: &JsonValue, dir: &DirMeta) -> Result<Vec<Entry>, Exn<RepoError>> {
+    let files = json
+        .get("flatFileStructure")
+        .and_then(JsonValue::as_array)
+        .ok_or_else(|| RepoError {
+            message: "field with key 'flatFileStructure' does not resolve to an json array"
+                .to_string(),
+        })?;
+
+    let license: String = json_extract(json, "usageLicense.iconCode").or_raise(|| RepoError {
+        message: "fail to extracting 'dataFile.filename' as String from json".to_string(),
+    })?; // json.get("usageLicense").and_then(|d| d.get("iconCode")).and_then(JsonValue::as_str);
+
+    let checksum: String = json_extract(json, "md5").or_raise(|| RepoError {
+        message: "fail to extracting 'usageLicense.iconCode' as String from json".to_string(),
+    })?;
+
+    let download_url = format!("{}/download", dir.api_url());
+
+    let download_url = Url::from_str(download_url.as_str()).or_raise(|| RepoError {
+        message: format!("invalid download url '{download_url}'"),
+    })?;
+
+    let checksum = Checksum::Md5(checksum);
+
+    let publication_date: String =
+        json_extract(json, "publicationDate").or_raise(|| RepoError {
+            message: "fail to extracting 'publicationDate' as String from json".to_string(),
+        })?;
+
+    let major_version: u64 =
+        json_extract(json, "versionNumber.majorVersion").or_raise(|| RepoError {
+            message: "fail to extracting 'versionNumber.majorVersion' as String from json"
+                .to_string(),
+        })?;
+
+    let minor_version: u64 =
+        json_extract(json, "versionNumber.minorVersion").or_raise(|| RepoError {
+            message: "fail to extracting 'versionNumber.minorVersion' as String from json"
+                .to_string(),
+        })?;
+
+    let version: String = format!("{}.{}", major_version, minor_version);
+
+    let mut entries = Vec::with_capacity(files.len());
+
+    for (_idx, filej) in files
+        .iter()
+        .filter_map(|filej| {
+            let s = filej.as_str()?; // or however you extract the string from your JSON value
+            let s = s.trim_end_matches('/');
+            let path = Path::new(s);
+            // Keep only paths that have a non-empty file extension
+            path.extension().map(|_| s)
+        })
+        .enumerate()
+    {
+        let endpoint = Endpoint {
+            parent_url: dir.api_url(),
+            key: None,
+        };
+
+        let guess = mime_guess::from_path(filej);
+
+        let file = FileMeta::new(
+            Some(filej.to_string()),
+            None,
+            dir.join(filej),
+            endpoint,
+            download_url.clone(),
+            None,
+            vec![checksum.clone()],
+            guess.first(),
+            Some(version.clone()),
+            Some(publication_date.clone()),
+            None,
+            license != "restricted",
+        );
+
+        entries.push(Entry::File(file));
+    }
+
+    Ok(entries)
+}
 
 #[derive(Debug)]
 pub struct SwissUbase {
@@ -59,11 +147,9 @@ impl DatasetBackend for SwissUbase {
             message: format!("fail GET {}, unable to convert to json", dir.api_url(),),
         })?;
 
-        println!("resp: {:?}", resp);
+        let entries = analyse_json(&resp, &dir)?;
 
-        let mut vec = Vec::new();
-
-        Ok(vec)
+        Ok(entries)
     }
 
     fn as_any(&self) -> &dyn Any {
@@ -78,11 +164,13 @@ mod tests {
 
     #[tokio::test]
     async fn test_list() {
-        let uuid = "41d06629-9e03-4631-933f-ff783a4d394c";
+        let uuid = "68afda6c-a438-45df-aecb-bf13b5dfa65d";
 
         let dataset = SwissUbase::new(uuid);
         let client = Client::new();
 
         let entries = dataset.list(&client, dataset.root_dir()).await.unwrap();
+
+        println!("{:#?}", entries);
     }
 }
