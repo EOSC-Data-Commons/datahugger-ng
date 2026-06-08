cc8fb9b67c56dde88a2aaab9cbd44fd38e4ee24c
Tobias Schweizer <tobias.schweizer@switch.ch>
2026-06-08 14:42:22 +0200

    test(python): add test for mdposit

--- python/tests/test_default.py
+++ python/tests/test_default.py
@@ -229,6 +229,17 @@ def test_crawl_swissubase():
     for i in ds.crawl_file():
         print(i)
 
+def test_crawl_mdposit():
+    ds = resolve(
+        "https://mdposit.mddbr.eu/#/id/MD-A0000A/overview"
+    )
+
+    for i in ds.crawl():
+        print(i)
+
+    for i in ds.crawl_file():
+        print(i)
+
 
 @pytest.mark.asyncio
 async def test_crawl_async():
